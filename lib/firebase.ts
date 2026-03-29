32f9a1f54b18102d6b5347bd4fedbde3fe68218f
samhith <samhith153@gmail.com>
2026-03-29 14:52:16 +0530

    portfolio ready for deployment

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAJZdCRPCgxTMJIrZu-EMdIa4fT1e9lLac",
-  authDomain: "thumbnail-portfolio-e4693.firebaseapp.com",
-  projectId: "thumbnail-portfolio-e4693",
-  storageBucket: "thumbnail-portfolio-e4693.firebasestorage.app",
-  messagingSenderId: "1030663114343",
-  appId: "1:1030663114343:web:82aa767de965d80f630ec2"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
